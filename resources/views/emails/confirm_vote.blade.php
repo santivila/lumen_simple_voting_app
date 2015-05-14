48cd60283a1b0f0da84fbd52c480c85f91c83b8e
Santi Vila <santi@lavitoverda.net>
2015-05-14 20:46:55 +0200

    Typo fixed

--- resources/views/emails/confirm_vote.blade.php
+++ resources/views/emails/confirm_vote.blade.php
@@ -1,6 +1,6 @@
-Siusplau confira el teu vot fent "clic" al següent enllaç:<br>
-Por favor confirma tu voto haciendo "clic" en el siguiente enlace:<br>
-Please confirm your vote clicking at the following link:<br>
+Siusplau confirma el teu vot fent "clic" al següent enllaç.<br>
+Por favor confirma tu voto haciendo "clic" en el siguiente enlace.<br>
+Please confirm your vote clicking on the link below.<br>
 <br/>
 <h4>{{ $confirmation_url }}</h4>
 <br/><br/>
