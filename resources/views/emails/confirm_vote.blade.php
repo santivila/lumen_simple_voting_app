a123c4a4ef69402ab8b1ebbb325455e29b4d2c01
Santi Vila <santi@lavitoverda.net>
2015-05-14 16:54:14 +0200

    Updated confirmation email in 3 languages

--- resources/views/emails/confirm_vote.blade.php
+++ resources/views/emails/confirm_vote.blade.php
@@ -1,5 +1,9 @@
+Siusplau confira el teu vot fent "clic" al següent enllaç:<br>
+Por favor confirma tu voto haciendo "clic" en el siguiente enlace:<br>
 Please confirm your vote clicking at the following link:<br>
-<h4>{{ $confirmation_url }}</h4>
 <br/>
-<h4>Thanks for your vote!</h4>
-This email has been entered on the voting process at ...
+<h4>{{ $confirmation_url }}</h4>
+<br/><br/>
+<h4>Gràcies per participar!. </h4>
+<h4>Gracias por participar!. </h4>
+<h4>Thanks for your vote!. </h4>
